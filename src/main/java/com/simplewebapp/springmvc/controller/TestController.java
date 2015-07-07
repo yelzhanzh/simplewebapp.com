fe9a938e516492317e962c6f7371a3aa70a9d8c0
yelzhanzh <yelzhan.zhangairn@formicary.net>
2015-07-07 10:27:00 +0100

    Add logback logging to the application

--- src/main/java/com/simplewebapp/springmvc/controller/TestController.java
+++ src/main/java/com/simplewebapp/springmvc/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.simplewebapp.springmvc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class TestController {
+  private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
   @RequestMapping(method = RequestMethod.GET)
   public  String sayHello(ModelMap model) {
